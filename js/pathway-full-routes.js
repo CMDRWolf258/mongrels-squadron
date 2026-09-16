b63ac775c78d34f79337a878688085789a62b7a9
CMDRWolf258 <michael.schroll@hotmail.com>
2026-09-16 16:49:25 -0500

    feat: suppress generic PvP card when full route is active

--- js/pathway-full-routes.js
+++ js/pathway-full-routes.js
@@ -4,6 +4,7 @@
 
   const fullPathwayRoots = new Map([
     ['PvE Combat', document.querySelector('[data-pve-pathway]')],
+    ['PvP', document.querySelector('[data-pvp-pathway]')],
     ['Background Simulation', document.querySelector('[data-bgs-pathway]')],
     ['Mining', document.querySelector('[data-mining-pathway]')],
     ['Trade & Hauling', document.querySelector('[data-trade-pathway]')],
