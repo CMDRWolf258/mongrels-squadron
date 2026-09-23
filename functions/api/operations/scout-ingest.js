6d50b3556d92335c18e579d834dd2ddae62eddc3
CMDRWolf258 <michael.schroll@hotmail.com>
2026-09-22 23:06:17 -0400

    Route Faction Alerts to member Mission Control

--- functions/api/operations/scout-ingest.js
+++ functions/api/operations/scout-ingest.js
@@ -130,7 +130,7 @@ export async function onRequestPost({ request, env }) {
       const bgsView=await loadBgsDiscordView(request,env);
       bgsDiscord=await syncBgsDiscordBoard(env,{
         view:bgsView,
-        missionControlUrl:new URL('/wolf-bgs/#faction-alerts',request.url).toString(),
+        missionControlUrl:new URL('/operations/#all-systems',request.url).toString(),
         createMissing:false,
       });
     }catch(error){
